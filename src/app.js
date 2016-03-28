204d5937e361b776d86ee0b48302dcd47cad1270
Vladimir Gorgievski <gorgievski.vladimir.l@gmail.com>
2016-03-28 17:09:55 +0200

    tes3

---
+++ src/app.js
@@ -0,0 +1,10 @@
+var express = require('express');
+var app = express();
+
+app.get('/', function (req, res) {
+  res.send('Hello World VLADIMIR FORM THE RIGHT PLACE!');
+});
+
+app.listen(process.env.PORT || 3000, function () {
+  console.log('Example app listening on port 3000!');
+});
